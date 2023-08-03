1322f174c663d9e01c5d1b10be3f8ac81ed4a763
Noella Mwanzia <noellamwanzia@gmail.com>
2023-08-03 12:24:40 +0300

    chore:update schema

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -12,6 +12,7 @@ export const createJournal = /* GraphQL */ `
       entry
       createdAt
       updatedAt
+      owner
       __typename
     }
   }
@@ -27,6 +28,7 @@ export const updateJournal = /* GraphQL */ `
       entry
       createdAt
       updatedAt
+      owner
       __typename
     }
   }
@@ -42,6 +44,7 @@ export const deleteJournal = /* GraphQL */ `
       entry
       createdAt
       updatedAt
+      owner
       __typename
     }
   }
