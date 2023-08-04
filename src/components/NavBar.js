45619bd3c19058ccbd45c789181e0792088a34ab
Noella Mwanzia <noellamwanzia@gmail.com>
2023-08-04 07:38:29 +0300

    chore: add sign out function to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,16 @@
-import styles from './navbar.module.css'
+import { Auth } from 'aws-amplify';
+import styles from './navbar.module.css';
 
-const NavBar = (props) => {
+
+const NavBar = () => {
     return (
         <>
         <header className={styles.navbar}>
         <h1 className={styles.title}> Random Journal App </h1>
+
+        <button className={styles.btn} type="button" onClick={() => Auth.signOut()}>
+            Sign out
+        </button>
         </header>
         </>
     )
