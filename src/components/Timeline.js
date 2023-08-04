014b02a610fed3a9c1caf7b00c0eaf70b30d1698
Noella Mwanzia <noellamwanzia@gmail.com>
2023-08-04 06:21:05 +0300

    fix: use dynamic journal data

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,48 +1,15 @@
 "use client"; 
-import { useState } from 'react';
 
 import Preview from './Preview';
 import styles from './timeline.module.css';
 
-const journalEntries = [
-    {createdOn: 'August 1, 2023', id: 1, entry:
-    <>
-    <h1>Journal entry one</h1> <p>Hello world</p>
-    </>},
-    {createdOn: 'August 2, 2023', id: 2, entry: 
-    <>
-    <h1>Journal entry two</h1> <p>Hello world</p>
-    </>},
-    {createdOn: 'August 2, 2023', id: 3, entry: 
-    <>
-    <h1>Journal entry three</h1> <p>Hello world</p>
-    </>}
-]
-
-const TimeLine = () => {
-
-    const [journals, setJournals] = useState(journalEntries);
-
-    const addJournal = () => {
-        setJournals([
-            ...journals,
-            {createdOn: 'August 3, 2023', id: 4, entry: 
-            <>
-            <h1>Journal entry Four</h1> <p>Hello world</p>
-            </>}
-        ])
-    }
+const TimeLine = ({journalEntries}) => {
 
     return (
         <div className={styles.timeline}>
         <h2 className={styles.header}>Past journal entries</h2>
         
-        {/* Extract component that previews journal entries */}
-        {journals.map(journal => <Preview key={journal.id} journal={journal}/>)}
-
-        {/* <button type="button" className={styles.btn} onClick={addJournal}>
-            New
-        </button> */}
+        {journalEntries.map(journal => <Preview key={journal.id} journal={journal}/>)}
         </div>
     )
 }
